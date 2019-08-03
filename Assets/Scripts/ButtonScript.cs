8effde05f85c4801882108bda44591c1d6919e03
Toreole <toreole.official@gmail.com>
2019-08-03 21:42:10 +0200

    oauibewtoa

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class ButtonScript : MonoBehaviour
 {
-  public void Activate()
-  {
-    print("Boop");
-  }
+    [SerializeField]
+    protected string handTag = "Hand";
+    [SerializeField]
+    protected Animator animator;
+    [SerializeField]
+    protected string clickTrigger = "Click";
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag(handTag))
+        {
+            animator.SetTrigger(clickTrigger);
+            print("collided with hand");
+        }
+    }
 }
